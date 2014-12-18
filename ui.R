0698d4a9da089d8eb85f43a8c640d1d6542d5762
Atsushi Mizumoto <atsushi@mizumot.com>
2014-12-18 09:51:31 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,4 +1,8 @@
 library(shiny)
+library(shinyAce)
+library(psych)
+library(beeswarm)
+
 
 
 shinyUI(bootstrapPage(
@@ -7,101 +11,214 @@ shinyUI(bootstrapPage(
     headerPanel("Non-parametric Tests"),
 
 
-    sidebarPanel(
+########## loading message #######################################
 
-        radioButtons("type", strong("Test type:"),
-                    list("Mann-Whitney U-test" = "mannwhitney",
-                         "Wilcoxon signed-rank test" = "wilcoxon",
-                         "Kruskal-Wallis test" = "kruskalwallis",
-                         "Friedman test" = "friedman"
-                        ),'Mann-Whitney U-test'
+tags$head(tags$style(type="text/css", "
+#loadmessage {
+position: fixed;
+top: 0px;
+left: 0px;
+width: 100%;
+padding: 10px 0px 10px 0px;
+text-align: center;
+font-weight: bold;
+font-size: 100%;
+color: #000000;
+background-color: #CCFF66;
+z-index: 105;
+}
+")),
+
+conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+tags$div("Loading...",id="loadmessage")),
+
+###################################################################
+
+
+
+    mainPanel(
+        tabsetPanel(
+
+
+# Mann-Whitney U-test (Comparing two independent conditions)
+
+        tabPanel("Mann-Whitney U-test",
+
+                h2("Mann-Whitney U-test"),
+
+                h4("Comparing two independent conditions"),
+
+                p('Note: Input values must be separated by tabs. Copy and paste from Excel/Numbers.'),
+
+                p(HTML("<b><div style='background-color:#FADDF2;border:1px solid black;'>Your data needs to have the header (variable names) in the first row.</div></b>")),
+
+                aceEditor("text1", value="Class\tScore\n1\t78\n1\t70\n1\t73\n1\t86\n1\t65\n1\t74\n1\t59\n1\t78\n1\t86\n1\t56\n1\t4\n1\t66\n1\t100\n1\t53\n1\t57\n2\t42\n2\t3\n2\t51\n2\t21\n2\t45\n2\t100\n2\t39\n2\t57\n2\t32\n2\t46\n2\t26\n2\t54\n2\t28\n2\t42\n2\t30", mode="r", theme="cobalt"),
+
+                br(),
+
+                h3("Basic statistics"),
+                verbatimTextOutput("MWU.bs.out"),
+
+                br(),
+
+                h3("Ranks"),
+                verbatimTextOutput("MWU.ranking.out"),
+
+                br(),
+
+                h3("Box plots with individual data points"),
+                plotOutput("MWU.boxPlot", width="80%"),
+
+                h3("Test result"),
+                verbatimTextOutput("MWU.test.out"),
+
+                br(),
+                br(),
+
+                strong('R session info'),
+                verbatimTextOutput("MWU.info.out")
+
         ),
 
-        br()
-
-    ),
-
-
-
-
-mainPanel(
-
-
-
-    tabsetPanel(
-
-        tabPanel("Main",
-
-            p('Note: Input values must be separated by tabs. Copy and paste from Excel/Numbers.'),
-
-            p(HTML("<b><div style='background-color:#FADDF2;border:1px solid black;'>Please make sure that your data includes the header (variable names) in the first row.</div></b>")),
-
-            aceEditor("text", value="Class\tScore\n1\t78\n1\t70\n1\t73\n1\t86\n1\t65\n1\t74\n1\t59\n1\t78\n1\t86\n1\t56\n1\t4\n1\t66\n1\t100\n1\t53\n1\t57\n2\t42\n2\t3\n2\t51\n2\t21\n2\t45\n2\t100\n2\t39\n2\t57\n2\t32\n2\t46\n2\t26\n2\t54\n2\t28\n2\t42\n2\t30",
-                mode="r", theme="cobalt"),
-
-            br(),
-
-            h3("Basic statistics"),
-            verbatimTextOutput("textarea.out"),
-
-            br(),
-
-            h3("Ranks"),
-            verbatimTextOutput("ranking.out"),
-
-            br(),
-
-            h3("Box plots with individual data points"),
-            downloadButton('downloadBoxPlot', 'Download the plot as pdf'),
-            plotOutput("boxPlot", width="80%"),
-
-            h3("Test result"),
-            verbatimTextOutput("test.out"),
-
-            br(),
-            br(),
-
-            strong('R session info'),
-            verbatimTextOutput("info.out")
-
-            ),
-
-
-
-    tabPanel("Input Examples",
-
-p('Note: Input values must be separated by tabs. Copy and paste from Excel/Numbers.'),
-
-p(HTML("<b><div style='background-color:#FADDF2;border:1px solid black;'>Please make sure that your data includes the header (variable names) in the first row.</div></b>")),
-
-br(),
-
-p(strong("Mann-Whitney U-test (Comparing two independent conditions)")),
-aceEditor("text1", value="Class\tScore\n1\t78\n1\t70\n1\t73\n1\t86\n1\t65\n1\t74\n1\t59\n1\t78\n1\t86\n1\t56\n1\t4\n1\t66\n1\t100\n1\t53\n1\t57\n2\t42\n2\t3\n2\t51\n2\t21\n2\t45\n2\t100\n2\t39\n2\t57\n2\t32\n2\t46\n2\t26\n2\t54\n2\t28\n2\t42\n2\t30", mode="r", theme="solarized_light"),
-
-
-br(),
-p(strong("Wilcoxon signed-rank test (Comparing two related conditions)")),
-aceEditor("text2", value="First\tSecond\n78\t42\n6\t3\n73\t51\n86\t21\n45\t45\n74\t98\n59\t59\n78\t57\n86\t84\n56\t46\n4\t26\n66\t54\n100\t28\n53\t42\n57\t30", mode="r", theme="solarized_light"),
-
-
-br(),
-p(strong("Kruskal-Wallis test (Differences between several independent groups)")),
-aceEditor("text3", value="Class\tScore\n1\t78\n1\t70\n1\t73\n1\t86\n1\t65\n1\t74\n1\t59\n1\t78\n1\t86\n1\t56\n1\t4\n1\t66\n1\t100\n1\t53\n1\t57\n2\t42\n2\t3\n2\t51\n2\t21\n2\t45\n2\t100\n2\t39\n2\t57\n2\t32\n2\t46\n2\t26\n2\t54\n2\t28\n2\t42\n2\t30\n3\t2\n3\t42\n3\t86\n3\t85\n3\t53\n3\t90\n3\t1\n3\t69\n3\t53\n3\t74\n3\t80\n3\t66\n3\t100\n3\t70\n3\t37",mode="r", theme="solarized_light"),
-
-
-br(),
-p(strong("Friedman test (Differences between several related groups)")),
-aceEditor("text4", value="First\tSecond\tThird\n78\t42\t42\n6\t3\t6\n73\t51\t86\n86\t21\t85\n45\t45\t53\n74\t98\t90\n59\t59\t1\n78\t57\t69\n86\t84\t53\n56\t46\t74\n4\t26\t80\n66\t54\t66\n100\t28\t100\n53\t42\t70\n57\t30\t37", mode="r", theme="solarized_light"),
-
-br()
-
-),
 
 
 
 
 
+# Wilcoxon signed-rank test (Comparing two related conditions)
+
+        tabPanel("Wilcoxon signed-rank test",
+
+                h2("Wilcoxon signed-rank test"),
+
+                h4("Comparing two related conditions"),
+
+                p('Note: Input values must be separated by tabs. Copy and paste from Excel/Numbers.'),
+
+                p(HTML("<b><div style='background-color:#FADDF2;border:1px solid black;'>Your data needs to have the header (variable names) in the first row.</div></b>")),
+
+                aceEditor("text2", value="First\tSecond\n78\t42\n6\t3\n73\t51\n86\t21\n45\t45\n74\t98\n59\t59\n78\t57\n86\t84\n56\t46\n4\t26\n66\t54\n100\t28\n53\t42\n57\t30", mode="r", theme="cobalt"),
+
+                br(),
+
+                h3("Basic statistics"),
+                verbatimTextOutput("WSR.bs.out"),
+
+                br(),
+
+                h3("Ranks"),
+                verbatimTextOutput("WSR.ranking.out"),
+
+                br(),
+
+                h3("Box plots with individual data points"),
+                plotOutput("WSR.boxPlot", width="80%"),
+
+                h3("Test result"),
+                verbatimTextOutput("WSR.test.out"),
+
+                br(),
+                br(),
+
+                strong('R session info'),
+                verbatimTextOutput("WSR.info.out")
+
+        ),
+
+
+
+
+
+
+# Kruskal-Wallis test (Differences between several independent groups)
+
+        tabPanel("Kruskal-Wallis test",
+
+                h2("Kruskal-Wallis test"),
+
+                h4("Differences between several independent groups"),
+
+                p('Note: Input values must be separated by tabs. Copy and paste from Excel/Numbers.'),
+
+                p(HTML("<b><div style='background-color:#FADDF2;border:1px solid black;'>Your data needs to have the header (variable names) in the first row.</div></b>")),
+
+                aceEditor("text3", value="Class\tScore\n1\t78\n1\t70\n1\t73\n1\t86\n1\t65\n1\t74\n1\t59\n1\t78\n1\t86\n1\t56\n1\t4\n1\t66\n1\t100\n1\t53\n1\t57\n2\t42\n2\t3\n2\t51\n2\t21\n2\t45\n2\t100\n2\t39\n2\t57\n2\t32\n2\t46\n2\t26\n2\t54\n2\t28\n2\t42\n2\t30\n3\t2\n3\t42\n3\t86\n3\t85\n3\t53\n3\t90\n3\t1\n3\t69\n3\t53\n3\t74\n3\t80\n3\t66\n3\t100\n3\t70\n3\t37",mode="r", theme="cobalt"),
+
+                br(),
+
+                h3("Basic statistics"),
+                verbatimTextOutput("KW.bs.out"),
+
+                br(),
+
+                h3("Ranks"),
+                verbatimTextOutput("KW.ranking.out"),
+
+                br(),
+
+                h3("Box plots with individual data points"),
+                plotOutput("KW.boxPlot", width="80%"),
+
+                h3("Test result"),
+                verbatimTextOutput("KW.test.out"),
+
+                br(),
+                br(),
+
+                strong('R session info'),
+                verbatimTextOutput("KW.info.out")
+
+        ),
+
+
+
+
+
+
+# Friedman test (Differences between several related groups)
+
+        tabPanel("Friedman test",
+
+                h2("Friedman test"),
+
+                h4("Differences between several related groups"),
+
+                p('Note: Input values must be separated by tabs. Copy and paste from Excel/Numbers.'),
+
+                p(HTML("<b><div style='background-color:#FADDF2;border:1px solid black;'>Your data needs to have the header (variable names) in the first row.</div></b>")),
+
+                aceEditor("text4", value="First\tSecond\tThird\n78\t42\t42\n6\t3\t6\n73\t51\t86\n86\t21\t85\n45\t45\t53\n74\t98\t90\n59\t59\t1\n78\t57\t69\n86\t84\t53\n56\t46\t74\n4\t26\t80\n66\t54\t66\n100\t28\t100\n53\t42\t70\n57\t30\t37", mode="r", theme="cobalt"),
+
+                br(),
+
+                h3("Basic statistics"),
+                verbatimTextOutput("Friedman.bs.out"),
+
+                br(),
+
+                h3("Ranks"),
+                verbatimTextOutput("Friedman.ranking.out"),
+
+                br(),
+
+                h3("Box plots with individual data points"),
+                plotOutput("Friedman.boxPlot", width="80%"),
+
+                h3("Test result"),
+                verbatimTextOutput("Friedman.test.out"),
+
+                br(),
+                br(),
+
+                strong('R session info'),
+                verbatimTextOutput("Friedman.info.out")
+
+        ),
+
+
+
+
+# About
 
         tabPanel("About",
 
@@ -116,6 +233,7 @@ br()
             code('library(shiny)'),br(),
             code('library(shinyAce)'),br(),
             code('library(psych)'),br(),
+            code('library(beeswarm)'),br(),
 
             br(),
 
@@ -140,7 +258,7 @@ br()
             'written by Dr. Jenifer Larson-Hall.'),
 
             p('Also, if you are a cool Mac user and want to use R with GUI,',
-            a("MacR", href="http://www.urano-ken.com/blog/2013/02/25/installing-and-using-macr/", target="_blank"),
+            a("MacR", href="https://sites.google.com/site/casualmacr/home", target="_blank"),
             'is defenitely the way to go!'),
 
             br(),
@@ -158,7 +276,7 @@ br()
 
             p(br())
 
-)
-)
-)
+        )
+    )
+    )
 ))
